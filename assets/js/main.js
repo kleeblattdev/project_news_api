8821a4ffa6bb1730dca791875907c05067bc9170
karinlee <karinlee92@gmail.com>
2023-02-16 14:39:21 +0100

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+
+
+fetch('https://newsapi.org/v2/top-headlines?country=de&apiKey=2731200c9f6d4b85a5c4165c5d639e92')
+
+.then(response => response.json())
+.then((data) => {
+    console.log(data)
+
+    data.articles.forEach((article) =>{ 
+
+        const urlToImage = article.urlToImage;
+        const title = article.title;
+        const description = article.description;
+        const publishedAt = article.publishedAt;
+        const url = article.url;
+
+        const newsArticle = document.createElement('article');
+        const newsImg = document.createElement('img');
+        newsImg.src = `${urlToImage}`;
+        const newsHeadline = document.createElement('h2');
+        newsHeadline.innerText = title;
+        const newsDescription = document.createElement('p');
+        newsDescription.innerText = description;
+        const newsPublished = document.createElement('p');
+        newsPublished.classList.add('publishedAt');
+        newsPublished.innerText = publishedAt.slice(0,10);
+        const newsReadMore = document.createElement('a');
+        newsReadMore.innerHTML = "READ MORE";
+        newsReadMore.href = `${url}`;
+
+        newsArticle.appendChild(newsImg)
+        newsArticle.appendChild(newsHeadline)
+        newsArticle.appendChild(newsDescription)
+        newsArticle.appendChild(newsPublished)
+        newsArticle.appendChild(newsReadMore);
+
+        document.querySelector('section').appendChild(newsArticle);
+    })
+
+
+
+})
